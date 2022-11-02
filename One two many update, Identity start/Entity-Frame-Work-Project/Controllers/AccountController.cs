b4a8da03e06de462d42eda5f4f3d08a66a143741
javidancode <javidanig@code.edu.az>
2022-11-03 02:25:19 +0400

    Added One two many update, Identity start

---
+++ One two many update, Identity start/Entity-Frame-Work-Project/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity_Frame_Work_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
